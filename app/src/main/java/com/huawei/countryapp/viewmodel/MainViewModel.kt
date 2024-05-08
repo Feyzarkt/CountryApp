c1c04251c60d450750820f6c3e48365ae8ac5c17
f00834653 <feyza.urkut1@huawei.com>
2024-05-08 08:32:02 +0300

    Add room functions

--- app/src/main/java/com/huawei/countryapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/huawei/countryapp/viewmodel/MainViewModel.kt
@@ -1,21 +1,35 @@
 package com.huawei.countryapp.viewmodel
 
+import android.app.Application
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.huawei.countryapp.database.CountryDao
+import com.huawei.countryapp.database.CountryDatabase
 import com.huawei.countryapp.model.Country
 import com.huawei.countryapp.service.CountryAPIService
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     private val countryAPI = CountryAPIService()
 
     val countryData = MutableLiveData<List<Country>>()
     val countryLoad = MutableLiveData<Boolean>()
     val countryError = MutableLiveData<Boolean>()
+
+    private var countryDatabase: CountryDatabase? = null
+    private var countryDao: CountryDao? = null
+    val country = MutableLiveData<Country>()
+    init {
+        countryDatabase = CountryDatabase.getInstance(application)
+        countryDao = countryDatabase?.countryDao()
+    }
 
     fun getDataFromAPI(){
         countryLoad.value = true
@@ -36,4 +50,11 @@ class MainViewModel : ViewModel() {
 
     }
 
+    fun insertAll(list: List<Country>) = viewModelScope.launch {
+        countryDao?.insertAll(list)
+    }
+
+    fun findByName (name:String) = viewModelScope.launch {
+        country.value = countryDao?.findByName(name)
+    }
 }
